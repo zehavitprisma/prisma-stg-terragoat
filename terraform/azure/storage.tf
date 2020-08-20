8e967443f2ccb177562205be0ddb81d4424aef0d
Barak Schoster Goihman <schosterbarak@gmail.com>
2020-08-20 23:34:18 +0300

    Update storage.tf

--- terraform/azure/storage.tf
+++ terraform/azure/storage.tf
@@ -10,6 +10,37 @@ resource "azurerm_managed_disk" "example" {
   }
 }
 
+
+resource "azurerm_storage_account" "example2" {
+  name                     = "tgsa${var.environment}${random_integer.rnd_int.result}"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  queue_properties {
+    logging {
+      delete                = false
+      read                  = false
+      write                 = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+    hour_metrics {
+      enabled               = true
+      include_apis          = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+    minute_metrics {
+      enabled               = true
+      include_apis          = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+  }
+}
+
+
 resource "azurerm_storage_account" "example" {
   name                     = "tgsa${var.environment}${random_integer.rnd_int.result}"
   resource_group_name      = azurerm_resource_group.example.name

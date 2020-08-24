2901fa56aa4149975d28173faf1bf7d103c3586a
Barak Schoster Goihman <schosterbarak@gmail.com>
2020-08-24 21:13:57 +0300

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -12,6 +12,18 @@ resource "aws_s3_bucket" "data" {
   }
 }
 
+
+resource "aws_s3_bucket" "data_prod" {
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+  }
+}
+
+
 resource "aws_s3_bucket_object" "data_object" {
   bucket = aws_s3_bucket.data.id
   key    = "customer-master.xlsx"

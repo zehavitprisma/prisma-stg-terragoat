c4bfc4ff55df5cc6231f7a6f055d1c3a01504ea7
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2020-10-22 15:36:17 -0500

    Create tests3.tf

---
+++ terraform/aws/tests3.tf
@@ -0,0 +1,19 @@
+resource "aws_s3_bucket_object" "data_object2" {
+  bucket = aws_s3_bucket.data.id
+  key    = "customer-master.xlsx"
+  source = "resources/customer-master.xlsx"
+  tags = {
+    Name        = "${local.resource_prefix.value}-customer-master"
+    Environment = local.resource_prefix.value
+  }
+}
+
+resource "aws_s3_bucket_object" "data_object3" {
+  bucket = aws_s3_bucket.data.id
+  key    = "customer-master.xlsx"
+  source = "resources/customer-master.xlsx"
+  tags = {
+    Name        = "${local.resource_prefix.value}-customer-master"
+    Environment = local.resource_prefix.value
+  }
+}

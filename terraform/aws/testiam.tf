da6bb4b3e516fb99c766ff8d55787484e3d01154
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2020-10-23 10:18:43 -0500

    Create testiam.tf

---
+++ terraform/aws/testiam.tf
@@ -0,0 +1,50 @@
+resource "aws_iam_user_policy" "test" {
+  name = "test-ci"
+  user = "test"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ecs:ListTaskDefinitions",
+                "ecs:RegisterTaskDefinition"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "iam:PassRole"
+            ],
+            "Resource": "arn:aws:iam::123456789012:role/ecsTaskExecutionRole"
+        },
+        {
+            "Effect":"Allow",
+            "Action":[
+               "ecr:GetAuthorizationToken"
+            ],
+            "Resource":"*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ecr:GetAuthorizationToken",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:InitiateLayerUpload",
+			    "ecr:UploadLayerPart",
+			    "ecr:CompleteLayerUpload",
+			    "ecr:PutImage"
+            ],
+            "Resource": [
+                "arn:aws:ecr:ap-northeast-1:123456789012:repository/dataflow"
+            ]
+        }
+    ]
+}
+EOF
+}

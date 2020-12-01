5108ba96a5dcfc5855d038838846d73f5329a9cf
Guy Sulema <48174831+guy-suli@users.noreply.github.com>
2020-12-01 13:35:08 +0200

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -23,6 +23,8 @@ resource "aws_s3_bucket_object" "data_object" {
 }
 
 resource "aws_s3_bucket" "financials" {
+  
+  
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning

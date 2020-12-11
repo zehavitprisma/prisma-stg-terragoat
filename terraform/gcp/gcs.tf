b8ab62b623b154e1c47f8263f406f41ab7af6757
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2020-12-11 11:19:26 -0600

    Update gcs.tf

--- terraform/gcp/gcs.tf
+++ terraform/gcp/gcs.tf
@@ -13,3 +13,8 @@ resource "google_storage_bucket" "internal_storage" {
   name          = "terragoat-internal"
   force_destroy = true
 }
+
+resource "google_storage_bucket" "internal_storage2" {
+  name          = "terragoat-internal2"
+  force_destroy = true
+}

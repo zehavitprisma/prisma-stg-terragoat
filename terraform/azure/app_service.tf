27d534b4994dfddaa9f7df9ab01f447613313394
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2021-01-21 15:17:16 -0600

    Update app_service.tf

--- terraform/azure/app_service.tf
+++ terraform/azure/app_service.tf
@@ -32,3 +32,14 @@ resource azurerm_app_service "app-service2" {
   }
 }
 
+resource azurerm_app_service "app-service3" {
+  app_service_plan_id = azurerm_app_service_plan.example.id
+  location            = var.location
+  name                = "terragoat-app-service-${var.environment}${random_integer.rnd_int.result}-3"
+  resource_group_name = azurerm_resource_group.example.name
+  https_only          = false
+  site_config {
+    min_tls_version = "1.1"
+  }
+}
+

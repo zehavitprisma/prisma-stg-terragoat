1c417786a6ff9d03328843fe86123f08e042eb51
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2021-01-22 13:20:58 -0600

    Update s3v1.tf

--- s3v1.tf
+++ s3v1.tf
@@ -7,14 +7,4 @@ resource "aws_s3_bucket" "b" {
     Environment = "Dev"
   }
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-  versioning {
-    enabled = true
-  }
 }

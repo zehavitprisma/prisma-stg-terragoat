f325af7916dd1f837d1d7634bc11a51959e75d1c
Mike Urbanski <mike@bridgecrew.io>
2021-02-10 18:03:40 -0600

    modify s3

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -39,6 +39,20 @@ resource "aws_s3_bucket" "financials" {
 
 }
 
+resource "aws_s3_bucket" "f2" {
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-f2"
+  acl           = "private"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-f2"
+    Environment = local.resource_prefix.value
+  }
+
+}
+
 resource "aws_s3_bucket" "operations" {
   # bucket is not encrypted
   # bucket does not have access logs

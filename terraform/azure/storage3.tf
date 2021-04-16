8c26325b5f41b196e544f2fe73f399c1cb7c0e75
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2021-04-16 13:37:05 -0500

    Create storage3.tf

---
+++ terraform/azure/storage3.tf
@@ -0,0 +1,8 @@
+resource "azurerm_managed_disk" "example" {
+  name                 = "terragoat-disk-${var.environment}"
+  location             = var.location
+  resource_group_name  = azurerm_resource_group.example.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 1
+}

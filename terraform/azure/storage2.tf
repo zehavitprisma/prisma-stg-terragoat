8851942bb77160532b6644eb950f449e89996308
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2021-04-20 14:02:00 -0500

    Create storage2.tf

---
+++ terraform/azure/storage2.tf
@@ -0,0 +1,8 @@
+resource "azurerm_storage_account" "storage1" {
+  name                     = "storage1"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+

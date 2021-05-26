d585e56769e4880418fb25b18c7d1bead1971772
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2021-05-26 09:13:43 -0500

    Update storage2.tf

--- terraform/azure/storage2.tf
+++ terraform/azure/storage2.tf
@@ -1,7 +1,7 @@
-resource "azurerm_storage_account" "storage1" {
-  name                     = "storage1"
+resource "azurerm_storage_account" "data2" {
+  name                     = "tgsa${var.environment}${random_integer.rnd_int.result}"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
   account_replication_type = "GRS"
 }
-

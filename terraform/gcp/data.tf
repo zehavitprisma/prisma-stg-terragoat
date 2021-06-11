e5dad69136650eb18c8378382ea5b34dc169da34
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2021-06-11 12:13:24 -0500

    Create data.tf

---
+++ terraform/gcp/data.tf
@@ -0,0 +1,19 @@
+resource google_sql_database_instance "master_instance" {
+  name             = "terragoat-${var.environment}-master"
+  database_version = "POSTGRES_11"
+  region           = var.region
+
+  settings {
+    tier = "db-f1-micro"
+    ip_configuration {
+      ipv4_enabled = true
+      authorized_networks {
+        name  = "WWW"
+        value = "0.0.0.0/0"
+      }
+    }
+    backup_configuration {
+      enabled = false
+    }
+  }
+}

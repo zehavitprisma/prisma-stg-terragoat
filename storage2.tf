7eec398b22f22d0106450eb8ebfc534499ad5a06
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2021-06-24 12:23:02 -0500

    Update storage2.tf

--- storage2.tf
+++ storage2.tf
@@ -4,4 +4,5 @@ resource "azurerm_storage_account" "example" {
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
+  min_tls_version      = "TLS1_0"
 }

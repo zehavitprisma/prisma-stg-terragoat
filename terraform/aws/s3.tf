25cbb58ff66ccb03b365bd0f599edac8f33c89f6
Mahesh Mani <manimahesh1@gmail.com>
2021-07-07 08:41:04 -0700

    Added Encryption to S3 Bucket

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -112,6 +112,7 @@ resource "aws_s3_bucket" "data_science" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
+        
         sse_algorithm = "AES256"
       }
     }

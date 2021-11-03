3609688009492b7a4b64a7b647a152aef8661efa
Brad Dausses <bdausses@chef.io>
2021-11-03 14:18:44 -0400

    Update terraform/aws/s3test.tf
    Co-authored-by: bridgecrew[bot] <60663194+bridgecrew[bot]@users.noreply.github.com>

--- terraform/aws/s3test.tf
+++ terraform/aws/s3test.tf
@@ -1,4 +1,11 @@
 resource "aws_s3_bucket" "data" {
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
   # bucket is public
   # bucket is not encrypted
   # bucket does not have access logs

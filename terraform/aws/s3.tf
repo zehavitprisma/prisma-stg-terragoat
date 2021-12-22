2fb8024739cbcc06027c6db5242ad09afd694830
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2021-12-22 09:56:18 -0600

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -1,3 +1,17 @@
+resource "aws_s3_bucket" "data2" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data2"
+  acl           = "public-read"
+  force_destroy = true
+  tags = {
+    Name                 = "${local.resource_prefix.value}-data"
+    Environment          = local.resource_prefix.value
+  }
+}
+
 resource "aws_s3_bucket" "data" {
   # bucket is public
   # bucket is not encrypted

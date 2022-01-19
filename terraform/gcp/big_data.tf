381a9f903b7b8095b1f3846a24cab64008c72089
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2022-01-19 09:31:21 -0600

    Update big_data.tf

--- terraform/gcp/big_data.tf
+++ terraform/gcp/big_data.tf
@@ -18,6 +18,26 @@ resource google_sql_database_instance "master_instance" {
   }
 }
 
+resource google_sql_database_instance "master_instance2" {
+  name             = "terragoat-${var.environment}-master2"
+  database_version = "POSTGRES_11"
+  region           = var.region
+
+  settings {
+    tier = "db-f1-micro"
+    ip_configuration {
+      ipv4_enabled = true
+      authorized_networks {
+        name  = "WWW"
+        value = "0.0.0.0/0"
+      }
+    }
+    backup_configuration {
+      enabled = false
+    }
+  }
+}
+
 resource google_bigquery_dataset "dataset" {
   dataset_id = "terragoat_${var.environment}_dataset"
   access {

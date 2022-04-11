71c18ebe711e2c1ab6b7df906e4511b175449e0b
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2022-04-11 11:22:30 -0500

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -23,6 +23,16 @@ resource "aws_s3_bucket" "data" {
   }
 }
 
+resource "aws_s3_bucket" "data2" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read"
+  force_destroy = true
+}
+
 resource "aws_s3_bucket_object" "data_object" {
   bucket = aws_s3_bucket.data.id
   key    = "customer-master.xlsx"
